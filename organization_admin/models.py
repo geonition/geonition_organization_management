2f72b47d795d1e9a163c9bd25ecad870b0349536
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-09-18 15:06:19 +0300

    the initial view of how the modles could work added

--- organization_admin/models.py
+++ organization_admin/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.sites.models import Site as Site_django
 
-# Create your models here.
+class Organization(models.Model):
+    """
+    Describing an Organization that uses geonition services
+    """
+    name = models.CharField(max_length=50,
+                            primary_key=True)
+    
+class Site(models.Model):
+    """
+    An Organizations site and definitions of the site
+    specific settings.
+    """
+    organization = models.ForeignKey(Organization)
+    site = models.OneToOneField(Site_django)
+    
+    #site specific settings
+    apps_in_use = models.ManyToManyField(Application)
+    database = models.ForeignKey(Database)
+    history = models.BooleanField()
+    mongodb = models.BooleanField()
+    
+    class Meta:
+        unique_together = (("organization", "site"),)
+
+class Application(models.Model):
+    """
+    An application service provided by geonition
+    """
+    name = models.CharField(max_length=50,
+                            primary_key=True)
+    
+    
+class Database(models.Model):
+    """
+    Should support modification of the following fields:
+    
+    'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': '',                      # Or path to database file if using sqlite3.
+    'USER': '',                      # Not used with sqlite3.
+    'PASSWORD': '',                  # Not used with sqlite3.
+    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',
+    
+    """
+    
+    name = models.CharField(max_length=30)
+    host = models.IPAddressField()
+    port = models.PositiveIntegerField()
